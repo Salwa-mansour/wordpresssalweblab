97175f118a88f5341b9a0773cc97d5e9192bbbda
Salwa-Mansour <alqadasisal@gmail.com>
2022-03-28 07:08:01 +0300

    comments added

--- js/hand.js
+++ js/hand.js
@@ -5,25 +5,23 @@ const salamHand = document.querySelector('.shake-hands');
 const salamText = document.querySelector('.meet');
 
 
-meeting();
+greeting();
 
-function meeting() {
+function greeting() {
    
     let isMoving = 0;
 
     // interval1 = setInterval(salamReset, 3000);
 
-    yourHand.addEventListener("click",alSalamAlaikum )
+    yourHand.addEventListener("click",alSalamAlaikum );
 
     function alSalamAlaikum() {
         
-        
-
         // setInterval(function(){window.requestAnimationFrame(salamReset)} ,4000)  
         isMoving = 1;
-        yourHand.classList.remove('salam-animation-reverce')
+        yourHand.classList.remove('salam-animation-reset')
         yourHand.classList.add('salam-animation');
-        myHand.classList.remove('salam-animation-reverce');
+        myHand.classList.remove('salam-animation-reset');
         myHand.classList.add('salam-animation');
         salamHand.classList.add('salam-animation');
         salamText.classList.add('salam-animation');
@@ -42,21 +40,23 @@ function meeting() {
     function salamReset(){
        
         isMoving =0;
-        yourHand.classList.add('salam-animation-reverce')
+        yourHand.classList.add('salam-animation-reset')
         yourHand.classList.remove('salam-animation');
         
-        myHand.classList.add('salam-animation-reverce');
+        myHand.classList.add('salam-animation-reset');
         myHand.classList.remove('salam-animation');
         salamHand.classList.remove('salam-animation');
         salamText.classList.remove('salam-animation');
         if (isMoving===0) {
-          console.log('salamReset')
+        //   console.log('salamReset')
             arrowPointer.style.animation='arrow-bounce .5s ease .1s infinite';
             
         } 
         //    yourHand.classList.remove('salam-animation-reverce');
         //    myHand.classList.remove('salam-animation-reverce');
         // window.requestAnimationFrame(salamReset)
+        // -- clearing the interval so the reset function doesn't conflict with user click at some point 
+        // AKA performing the reset just once after the animation is performed
         clearInterval(salamAnimationInerval);
     }
    
